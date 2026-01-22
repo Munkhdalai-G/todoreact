f1152d3f173374f6b03da6251d3ee66378faff0a
Munkhdalai-G <Bylzuuhai7@gmail.com>
2026-01-22 14:20:38 +0800

    done3

--- src/app/onAdd.jsx
+++ src/app/onAdd.jsx
@@ -5,6 +5,7 @@ export default function OnAdd({ handleInputChange, onAddTodo, value }) {
       <input
         className="border-gray-300 border rounded-md w-100 pl-2"
         type="text"
+        maxLength={20}
         placeholder="  Add a new task..."
         value={value}
         onChange={handleInputChange}
